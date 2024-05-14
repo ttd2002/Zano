58e1da7f9cf85f1bfb29dd84b0d186bc97df7cdf
NguyenTienDat-GTR <80930464+NguyenTienDat-GTR@users.noreply.github.com>
2024-05-14 20:41:43 +0700

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-require("dotenv").config();
-const { register, login } = require("../controllers/authController");
-
-router.post("/register", register);
-router.post("/login", login);
-
-module.exports = router;
